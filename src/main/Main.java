f0fbd7efed3df822d16aa85543acc1440d52a0ad
João Victor <jvs.devsilva@gmail.com>
2025-03-28 11:21:39 -0300

    add Menu integration in Main class to display user menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,11 @@
 package main;
+import main.service.Menu;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        menu.exibirMenu();
+    }
 }
